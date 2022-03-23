81b1918741068c0e76e05fb40a2cbabfc4f5c6b9
Govind Mishra <govindkm.sv@gmail.com>
2022-03-23 17:11:17 +0530

    ContentChild usage explained

--- 2. DataBinding/cmp-databinding-start/cmp-databinding-start/src/app/server-element/server-element.component.ts
+++ 2. DataBinding/cmp-databinding-start/cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -1,4 +1,4 @@
-import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, Input, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, ContentChild, DoCheck, ElementRef, Input, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
 import { element } from './server-element.interface';
 
 @Component({
@@ -8,6 +8,10 @@ import { element } from './server-element.interface';
 })
 export class ServerElementComponent implements OnInit, DoCheck, OnChanges, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy  {
   @Input('alias') element: element;
+
+  //Contentchild is used when the we want to access dom elementref which is passed as content to the component dom and we are using ng-content directive to render it.
+  @ContentChild('paragraphContent') contentElement: ElementRef;
+
   //Lifecycle hooks in order of their executions
   //-------------------------------------------------------------------
   // constructor is the first method to get invoked and gets invoked only once when object instantiates
@@ -22,6 +26,7 @@ export class ServerElementComponent implements OnInit, DoCheck, OnChanges, After
   // ngOnInit is called once the component is initialized 
   ngOnInit(): void {
     console.warn('ngOnInit Method not implemented.');
+    console.log(`%c${this.contentElement}`, "background-color:green;");
   }
   // ngDoCheck is called after the ngOnInit hook and is called whenever any event occurs on the dom. It is the best method to manually check the events
   ngDoCheck(): void {
@@ -30,6 +35,7 @@ export class ServerElementComponent implements OnInit, DoCheck, OnChanges, After
   // Called after content (ng-content) has been projected into view
   ngAfterContentInit(): void {
     console.warn('ngAfterContentInit Method not implemented.');
+    console.log(`%c${this.contentElement}`, "background-color:green;")
   }
   // Called every time the projected content has been checked
   ngAfterContentChecked(): void {
@@ -48,7 +54,6 @@ export class ServerElementComponent implements OnInit, DoCheck, OnChanges, After
     console.warn('ngOnDestroy Method not implemented.');
   }  
   //
-
   // ------------------------------------------------------------------
 
 
