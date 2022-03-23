b0d37b0b38db78ef6baff56fe77fe92b66908cb9
Govind Mishra <govindkm.sv@gmail.com>
2022-03-24 00:37:02 +0530

    Recipe Book: Toggle From anywhere

--- recipe-book--project/src/app/directives/dropdown-helper.directive.ts
+++ recipe-book--project/src/app/directives/dropdown-helper.directive.ts
@@ -4,11 +4,9 @@ import { Directive, ElementRef, HostBinding, HostListener } from '@angular/core'
   selector: '[appDropdownHelper]'
 })
 export class DropdownHelperDirective {
-  @HostBinding('class') classList: string;
-  constructor(private elementRef:ElementRef) { }
-
-  @HostListener('click')
-  toggleDropdown(){
-    this.elementRef.nativeElement.classList.toggle('open');
+  @HostBinding('class.open') isOpen = false;
+  @HostListener('document:click', ['$event']) toggleOpen(event: Event) {
+    this.isOpen = this.elRef.nativeElement.contains(event.target) ? !this.isOpen : false;
   }
+  constructor(private elRef: ElementRef) {}
 }
