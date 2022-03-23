df1f443d43d6d8902d9c9b656b03c22ac098970b
Govind Mishra <govindkm.sv@gmail.com>
2022-03-23 17:38:53 +0530

    Recipe Book: Navigation using ngIf and events

--- 1. Basics/recipe-book--project/src/app/app.component.ts
+++ 1. Basics/recipe-book--project/src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'recipe-book--project';
+  view='';
+  changeView(event){
+    this.view = event;
+  }
 }
--- 1. Basics/recipe-book--project/src/app/header/header.component.ts
+++ 1. Basics/recipe-book--project/src/app/header/header.component.ts
@@ -1,10 +1,15 @@
-import { Component } from "@angular/core";
-
+import { Component, EventEmitter, Output } from "@angular/core";
 @Component({
     selector:"app-header",
     templateUrl:"./header.component.html",
     styleUrls:["./header.component.css"]
 })
 export class HeaderComponent{
+@Output('navItem') item: EventEmitter<Component> = new EventEmitter<Component>();
+constructor(){}
+navigate(event){
+    console.log(event);
+    this.item.emit(event);
+}
 
 }
