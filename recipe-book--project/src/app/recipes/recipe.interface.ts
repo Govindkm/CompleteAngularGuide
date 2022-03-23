1b5ca2ce76b02a7f48467ef3261ff7960d43b1df
Govind Mishra <govindkm.sv@gmail.com>
2022-03-23 19:04:48 +0530

    Update Folder Structure

---
+++ recipe-book--project/src/app/recipes/recipe.interface.ts
@@ -0,0 +1,10 @@
+export interface Recipe{
+    name: string;
+    description: string;
+    imagePath?: string;
+}
+
+export interface Ingrediennt{
+    name: string;
+    amount: string;
+}
---
+++ recipe-book--project/src/app/recipes/recipes.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from './recipe.interface';
+
+@Component({
+  selector: 'app-recipes',
+  templateUrl: './recipes.component.html',
+  styleUrls: ['./recipes.component.css']
+})
+export class RecipesComponent implements OnInit {
+  selectedRecipe:Recipe = null;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onSelectRecipe(recipe:Recipe){
+    this.selectedRecipe=recipe;
+  }
+
+}
