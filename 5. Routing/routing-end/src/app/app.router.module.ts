0eec8546aa02901cbbb454b8359b8e4333fd3aa6
Govind Mishra <govindkm.sv@gmail.com>
2022-03-24 11:53:20 +0530

    Routing: Router Module Setup Done

---
+++ 5. Routing/routing-end/src/app/app.router.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { HomeComponent } from "./home/home.component";
+import { ServerComponent } from "./servers/server/server.component";
+import { ServersComponent } from "./servers/servers.component";
+import { UserComponent } from "./users/user/user.component";
+import { UsersComponent } from "./users/users.component";
+
+const routes:Routes = [
+    {path:'', component:HomeComponent},
+    {path:'servers', component:ServersComponent},
+    {path:'users', component:UsersComponent},
+]
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule{
+
+}
