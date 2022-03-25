cb42c8733ad3474bc418b09d3f274a4a06c7dba7
Govind Mishra <govindkm.sv@gmail.com>
2022-03-25 10:37:59 +0530

    Modification in Server Router Outlet

--- 5. Routing/routing-end/src/app/servers/edit-server/edit-server.component.ts
+++ 5. Routing/routing-end/src/app/servers/edit-server/edit-server.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 import { ServersService } from '../servers.service';
 
@@ -12,16 +12,20 @@ export class EditServerComponent implements OnInit {
   server: {id: number, name: string, status: string};
   serverName = '';
   serverStatus = '';
+  allowEdit=true;
 
-  constructor(private serversService: ServersService, private route:ActivatedRoute) { }
+  constructor(private serversService: ServersService, private route:ActivatedRoute, private router:Router) { }
 
   ngOnInit() {
     let snapshot = this.route.snapshot;
     console.log(snapshot.queryParams);
-    console.log(snapshot.params);
+    console.log(snapshot.fragment);
+    this.allowEdit = snapshot.queryParams['allowEdit'];
     this.server = this.serversService.getServer(+snapshot.params['id'] || 1);
     this.serverName = this.server.name;
     this.serverStatus = this.server.status;
+
+    this.router.routeReuseStrategy.shouldReuseRoute = () => false;
   }
 
   onUpdateServer() {
