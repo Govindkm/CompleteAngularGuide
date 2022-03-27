a63221de81578f839821c918ee0cc9a414e43890
Govind Mishra <govindkm.sv@gmail.com>
2022-03-27 10:27:59 +0530

    RecipeBook: Recipe Edit added

--- recipe-book--project/src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ recipe-book--project/src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeEditComponent implements OnInit {
 
-  constructor() { }
+  id:string='';
+  editMode:boolean= false;
+  constructor(private route:ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.route.params.subscribe((params)=>{
+      if(params['id']){
+        this.id = params['id'];
+        this.editMode = true; 
+      }
+    })
   }
 
 }
