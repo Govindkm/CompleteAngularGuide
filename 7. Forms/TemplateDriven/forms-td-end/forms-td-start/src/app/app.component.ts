9aa4851201b58b5cfe9a37ee06143fea39ab62a3
Govind Mishra <govindkm.sv@gmail.com>
2022-03-27 15:11:40 +0530

    ReactiveForm: Accessing form inside controller

--- 7. Forms/TemplateDriven/forms-td-end/forms-td-start/src/app/app.component.ts
+++ 7. Forms/TemplateDriven/forms-td-end/forms-td-start/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('myForm') form:NgForm;
   suggestUserName() {
     const suggestedName = 'Superuser';
+  }
+
+  onSubmit(){
+    console.log(this.form);
   }
 }
