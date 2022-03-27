e070f764cc91779ab0172ec34a85525123933e96
Govind Mishra <govindkm.sv@gmail.com>
2022-03-27 12:17:09 +0530

    Observable: Custom Observable(Interval)

--- 6. Observables/obs-01-end/src/app/home/home.component.ts
+++ 6. Observables/obs-01-end/src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { interval, Subscription } from 'rxjs';
+import { interval, Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -7,18 +7,35 @@ import { interval, Subscription } from 'rxjs';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit, OnDestroy {
-  subscription:Subscription;
+  subscription:Subscription[] = [];
   constructor() { }
 
   ngOnInit() {
     //Subscription should be closed otherwise it causes memory leaks which will end up utilizing the memory resources
-    this.subscription = interval(1000).subscribe((data)=>{
-      console.log(data);
-    })
+    this.subscription.push(interval(1000).subscribe((data)=>{
+      console.log('Interval : ', data);
+    }));
+
+    const customInterval = (timeout)=>{
+      return new Observable(observer=>{
+        let count=0;
+        setInterval(()=>{
+          observer.next(count--);
+        }, timeout)
+      })
+    }
+
+    //Custom interval
+    this.subscription.push(customInterval(1000).subscribe((data)=>{
+      console.log('Custom Interval : ', data);
+    }));
+
   }
 
   ngOnDestroy(): void {
-    this.subscription.unsubscribe();
+    this.subscription.forEach(sub=>{
+      sub.unsubscribe();
+    })
   }
 
 }
