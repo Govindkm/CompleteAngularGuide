4ec5010e0648d33698663b0fcacfa340a0057978
Govind Mishra <govindkm.sv@gmail.com>
2022-04-05 11:19:51 +0530

    Pipes: Passing parameters to the custom pipes

--- 8. Pipes/pipes-start/pipes-start/src/app/shorten.pipe.ts
+++ 8. Pipes/pipes-start/pipes-start/src/app/shorten.pipe.ts
@@ -5,7 +5,7 @@ import { Pipe, PipeTransform } from "@angular/core";
 export class ShortenPipe implements PipeTransform{
     transform(value: string, ...args: any[]) {
         if(value.length>10){
-            return value.slice(0, 10) + ' ...';
+            return value.slice(args[0], args[1]) + ' ...';
         }
         else
             return value;
