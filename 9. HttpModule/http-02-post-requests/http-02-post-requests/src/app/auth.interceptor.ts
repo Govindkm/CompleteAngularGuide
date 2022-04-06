6fc4d99b706433ea2c3b0d3c5655897ada992924
Govind Mishra <govindkm.sv@gmail.com>
2022-04-06 10:33:49 +0530

    Http: Using Tap on a response inside Interceptor

--- 9. HttpModule/http-02-post-requests/http-02-post-requests/src/app/auth.interceptor.ts
+++ 9. HttpModule/http-02-post-requests/http-02-post-requests/src/app/auth.interceptor.ts
@@ -1,6 +1,7 @@
-import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+import { HttpEvent, HttpEventType, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
+import { tap } from "rxjs/operators";
 
 @Injectable(
     {
@@ -10,6 +11,14 @@ import { Observable } from "rxjs";
 export class AuthInterceptor implements HttpInterceptor{
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         console.log('Intercepting the request: ', req);
-        return next.handle(req);
+        //Adding a auth key to the request object(check the network tab to see the added header auth)
+        const modifiedReq = req.clone({headers:req.headers.append('auth', 'key')});
+        console.log('Modified header in new request: ', modifiedReq);
+        return next.handle(modifiedReq).pipe(tap(event=>{
+            console.log(event);
+            if(event.type === HttpEventType.Response){
+                console.log('Response Recieved: ', event);
+            }
+        }))
     }
 }
